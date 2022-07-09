1399dbfbc50596f38cf673ba7172914a2ddf35bb
Nicolas Mahnic <nmahnic@pdssa.com.ar>
2022-07-09 19:26:48 -0300

    Test: GetMarvelItem

---
+++ app/src/test/java/com/nicomahnic/capgeminichallenge/domain/GetMarvelItemFromDBUseCaseTest.kt
@@ -0,0 +1,66 @@
+package com.nicomahnic.capgeminichallenge.domain
+
+import com.nicomahnic.capgeminichallenge.datasource.database.models.MarvelItemEntity
+import com.nicomahnic.capgeminichallenge.datasource.database.models.MarvelThumbnailEntity
+import com.nicomahnic.capgeminichallenge.models.MarvelItem
+import com.nicomahnic.capgeminichallenge.models.MarvelThumbnail
+import com.nicomahnic.capgeminichallenge.models.mapper.MarvelEntityItemMapper
+import com.nicomahnic.capgeminichallenge.models.mapper.MarvelEntityThumbnailMapper
+import com.nicomahnic.capgeminichallenge.repository.LocalRepository
+import io.mockk.MockKAnnotations
+import io.mockk.coEvery
+import io.mockk.coVerify
+import io.mockk.impl.annotations.RelaxedMockK
+import io.mockk.verify
+import kotlinx.coroutines.runBlocking
+import org.junit.Before
+import org.junit.Test
+
+class GetMarvelItemFromDBUseCaseTest {
+
+    @RelaxedMockK
+    private lateinit var localRepository: LocalRepository
+
+    private lateinit var marvelEntityItemMapper: MarvelEntityItemMapper
+    private lateinit var marvelEntityThumbnailMapper: MarvelEntityThumbnailMapper
+    lateinit var getMarvelItemFromDBUseCase: GetMarvelItemFromDBUseCase
+
+    @Before
+    fun onBefore(){
+        MockKAnnotations.init(this)
+        marvelEntityThumbnailMapper = MarvelEntityThumbnailMapper()
+        marvelEntityItemMapper = MarvelEntityItemMapper(marvelEntityThumbnailMapper)
+        getMarvelItemFromDBUseCase = GetMarvelItemFromDBUseCase(localRepository, marvelEntityItemMapper)
+    }
+
+    @Test
+    fun `when VM doesnt pass any id then return null`() = runBlocking {
+        //Given
+        coEvery { localRepository.getMarvelItem() } returns null
+
+        //When
+        getMarvelItemFromDBUseCase()
+
+        //Then
+        assert ( marvelEntityItemMapper.mapFromEntity() == null )
+
+    }
+
+    @Test
+    fun `when VM pass id 1 then return MarvelItem parsed`() = runBlocking {
+        //Given
+        val marvelThumbnailEntity = MarvelThumbnailEntity("Path", "jpg")
+        val marvelItemEntity = MarvelItemEntity(1, "Test1","Test1", marvelThumbnailEntity)
+        coEvery { localRepository.getMarvelItem() } returns marvelItemEntity
+
+        //When
+        getMarvelItemFromDBUseCase(1)
+
+        //Then
+        val marvelThumbnail = MarvelThumbnail("Path", "jpg")
+        val marvelItem = MarvelItem(1, "Test1","Test1", marvelThumbnail)
+        assert( marvelEntityItemMapper.mapFromEntity(marvelItemEntity) == marvelItem)
+
+    }
+
+}
